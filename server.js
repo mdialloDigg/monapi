9ba0be0048ca635689d567576cc0fcd5bf046af7
DIALLO <m.lamine.diallo@gmail.com>
2025-12-19 22:32:55 +0100

    🔧 Mise à jour du formulaire et de l'affichage des transferts

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
 
 // Afficher le formulaire HTML
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/users', (req, res) => {
-  res.sendFile(path.join(__dirname, 'users.html'));
+  res.sendFile(path.join(__dirname, 'public', 'users.html'));
 });
+
 
 // Connexion MongoDB
 mongoose.connect(
